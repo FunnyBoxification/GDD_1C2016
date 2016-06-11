5e26ba487c49eb6e9d2246a9d6687f0d80885453
FunnyBoxification <pablorom93@gmail.com>
2016-06-11 14:15:43 -0300

    Clase SuperGrid, gridview paginada y HistorialVendedor

---
+++ MercadoEnvio/WindowsFormsApplication1/Facturas/FacturasVendedor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MercadoNegocio;
+
+namespace WindowsFormsApplication1.Facturas
+{
+    public partial class FacturasVendedor : Form
+    {
+        public FacturasVendedor()
+        {
+            InitializeComponent();
+            var negocio = new HistorialVendedor(SqlServerDBConnection.Instance());
+            superGrid1.PageSize = 5;
+            superGrid1.SetPagedDataSource(negocio.searchFacturasAVendedor(-1, null,-1,-1,-1,-1), bindingNavigator1);
+        }
+
+        private void FacturasVendedor_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void superGrid1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
