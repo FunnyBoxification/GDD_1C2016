e5ad9b3cc1950af3b3c043c9b843a1c0687c3a05
FunnyBoxification <pablorom93@gmail.com>
2016-06-29 00:31:57 -0300

    asdas

---
+++ borro todo.sql
@@ -0,0 +1,50 @@
+GO
+	drop proc PMS.ALTA_CALIFICACION;
+	drop proc PMS.ALTA_COMPRAS
+	drop proc PMS.ALTA_EMPRESA
+	drop proc PMS.ALTA_FACTURA
+	drop proc PMS.ALTA_OFERTAS
+	drop proc PMS.ALTA_PUBLICACION
+	drop proc PMS.ALTA_ROL
+	drop proc PMS.ALTA_RUBRO
+	drop proc PMS.ALTA_USER
+	drop proc PMS.ALTA_USUARIO_CLIENTE
+	drop proc PMS.ALTA_USUARIO_EMPRESA
+	drop proc PMS.ALTA_CLIENTE
+	drop proc PMS.AumentarIntentosFallidos
+	drop proc PMS.BAJA_ROL
+	drop proc PMS.CreacionTabla
+	drop proc PMS.del_Rol
+	drop proc PMS.insert_Ro
+	drop proc PMS.LimpiarIntentos
+	drop proc PMS.MODIFICACION_ROLES
+	drop proc PMS.SUBASTAS_TERMIANDAS
+	drop proc PMS.upd_Rol
+
+	drop function PMS.getEstrellasDadas
+	drop function PMS.getUser
+	
+
+	drop table PMS.ITEMFACTURA
+	drop table PMS.FACTURAS
+	drop table PMS.OFERTAS
+	drop table PMS.FORMASDEPAGO
+	drop table PMS.ROLES_USUARIOS
+	drop table PMS.COMPRAS
+	drop table PMS.FUNCIONALIDES_ROLES
+	drop table PMS.FUNCIONALIDADES
+	drop table PMS.EMPRESAS
+	drop table PMS.CLIENTES
+	drop table PMS.CALIFICACIONES
+	drop table PMS.PUBLICACIONES
+	drop table PMS.ROLES
+	drop table PMS.RUBROS
+	drop table PMS.PUBLICACION_ESTADOS
+	drop table PMS.USUARIOS
+	drop table PMS.VISIBILIDADES
+	drop table PMS.TIPO_PUBLICACION
+
+	DROP TYPE [PMS].[Funcionalidades]
+	DROP SCHEMA [PMS]
+
+GO
