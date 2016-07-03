95c26f5251e773e680b7b5d9d829cc92e9ee7d2e
arilei <leibelariel@gmail.com>
2016-07-03 16:26:34 -0300

    controlar publicaciones
    da por terminadas publicaciones que tengan factura por venta

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -503,6 +503,10 @@ SET
 Descripcion='Activa'
 where Id_Estado=1;
 
+update PMS.PUBLICACIONES 
+set Id_Estado= 4
+where Id_Publicacion in (select f.Id_Publicacion from PMS.ITEMFACTURA f where Descripcion != 'Comision por venta')
+
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
